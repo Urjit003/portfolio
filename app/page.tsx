7649a88e7556861561ccefd954c22c1386b24a89
Dabhi Urjit <urjitdabhi123@gmail.com>
2024-08-22 20:08:14 +0530

    initial commint
    setted up the project with tailwindCSS

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>hello world</h1>
+      </div>
+    </main>
+  );
+};
+
+export default page;
